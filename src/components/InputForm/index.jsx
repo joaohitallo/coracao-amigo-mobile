bd8d9d56988f3b166a48e05c62743818eb343cd1
João Hitallo <joaohitallo97@gmail.com>
2022-08-16 17:55:27 -0300

    Build: adicionando biblioteca react-native-mask-input

--- src/components/InputForm/index.jsx
+++ src/components/InputForm/index.jsx
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 import { TextInputProps, Text } from 'react-native';
 import { Container, TextLabel, InputText } from './styles.js';
 
-export function InputForm({ value, label, ...rest }) {
+export function InputForm({ value, label, children, ...rest }) {
 
   return (
     <Container>
       <TextLabel>{label}</TextLabel>
-      <InputText
-        value={value}
-        {...rest}
-      />
+      {children ?
+        children
+        :
+        <InputText
+          value={value}
+          {...rest}
+        />}
+
     </Container>
   )
 }
